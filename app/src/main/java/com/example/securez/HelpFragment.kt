2f804f65b4fee19bc44898bd995ee0b2954581b9
Aditya Rajendra Alat <116866086+Shluch@users.noreply.github.com>
2023-08-08 19:36:29 +0530

    commit 2

---
+++ app/src/main/java/com/example/securez/HelpFragment.kt
@@ -0,0 +1,45 @@
+package com.example.securez
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HelpFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HelpFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_help, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HelpFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
